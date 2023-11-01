01be3082e7b102b2cff8ab482cb9f31db67a9a45
Rojo <rojo@rojo.dev>
2023-11-01 23:09:23 +0000

    Small refactor, variables in the right context

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
-def async_request(url):
+def async_request(url, headers, data):
   # Errors in thread need to be tried separately because they don't propagate to the main stack
   try:
-    headers = request.headers
     if 'Content-Type' not in headers:
             headers['Content-Type'] = 'application/json'  # Set a default Content-Type, if it's not included in the original request our request fails (Flask requires content type to be present by default)
 
-    res = requests.post(url, headers={'Content-Type': 'application/json'})
+    res = requests.post(url, headers=headers, json=data)
     res.raise_for_status()  # Raise an exception if the request was not successful (status code >= 400)
   except requests.exceptions.RequestException as e:
     logging.error(f"Failed to make a request to {url}: {e}") # Catch request errors separately
@@ -28,10 +27,11 @@ def async_request(url):
 def webhook(subpath):
   try:
     dst_url = os.environ.get("RELAY_DST_URL")
+    headers = request.headers
     data    = request.get_json()
     url     = f"{dst_url}/{subpath}"
 
-    thread  = threading.Thread(target=async_request, args=(url,))
+    thread  = threading.Thread(target=async_request, args=(url, headers, data))
     thread.start()
 
     return jsonify(success=True), 200
