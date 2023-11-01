2c7039e5eebc8ca9b3f269bded11f8f0eb5d1039
Rojo <rojo@rojo.dev>
2023-11-01 23:48:12 +0000

    Refactor app to allow requests without json content type

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import threading
 import requests
 import logging
+import ipdb
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
@@ -13,29 +14,30 @@ app = Flask(__name__)
 def async_request(url, headers, data):
   # Errors in thread need to be tried separately because they don't propagate to the main stack
   try:
-    if 'Content-Type' not in headers:
-            headers['Content-Type'] = 'application/json'  # Set a default Content-Type, if it's not included in the original request our request fails (Flask requires content type to be present by default)
-
     res = requests.post(url, headers=headers, json=data)
     res.raise_for_status()  # Raise an exception if the request was not successful (status code >= 400)
   except requests.exceptions.RequestException as e:
     logging.error(f"Failed to make a request to {url}: {e}") # Catch request errors separately
   except Exception as e:
-      logging.error(f"An error occurred in async_request: {e}") # Catch any other errors
+    logging.error(f"An error occurred in async_request: {e}") # Catch any other errors
 
 @app.route('/webhooks/<path:subpath>', methods=['POST'])
 def webhook(subpath):
   try:
     dst_url = os.environ.get("RELAY_DST_URL")
     headers = request.headers
-    data    = request.get_json()
     url     = f"{dst_url}/{subpath}"
+    data    = '{}'
+
+    if 'Content-Type' in headers:
+      data = request.get_json() # We skip this because otherwise flask complains about not having a content type
 
     thread  = threading.Thread(target=async_request, args=(url, headers, data))
     thread.start()
 
     return jsonify(success=True), 200
   except Exception as e:
+    ipdb.set_trace()
     logging.error(f"An error occurred in the main thread: {e}")
 
 if __name__ == '__main__':
