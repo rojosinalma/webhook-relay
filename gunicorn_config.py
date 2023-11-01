142e41c8d28fdc14d341771c50cc971698d8ec02
Rojo <rojo@rojo.dev>
2023-11-01 21:25:22 +0000

    small refactor of gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,10 +3,8 @@ import os
 
 load_dotenv()
 
-workers = 1  # You can adjust the number of worker processes
-env     = {
-    "RELAY_HOST": "0.0.0.0",
-    "RELAY_PORT": "50000"
-}
+workers   = 1  # You can adjust the number of worker processes
+accesslog = '-' # Access log to STDOUT
+timeout   = int(os.environ.get("RELAY_TIMEOUT", 5))
 
-bind = f"{os.environ['RELAY_HOST']}:{os.environ['RELAY_PORT']}"
+bind = f'{os.environ.get("RELAY_HOST", "0.0.0.0")}:{os.environ.get("RELAY_PORT", 50000)}'
