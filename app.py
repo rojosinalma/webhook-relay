b7b310f85d121170c274b96983321cafeb205e98
Rojo <rojo@rojo.dev>
2023-11-01 23:05:00 +0000

    App now passes headers and adds Content-Type if not present

--- app.py
+++ app.py
@@ -11,27 +11,32 @@ logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 
 def async_request(url):
+  # Errors in thread need to be tried separately because they don't propagate to the main stack
   try:
-    res = requests.post(url)
+    headers = request.headers
+    if 'Content-Type' not in headers:
+            headers['Content-Type'] = 'application/json'  # Set a default Content-Type, if it's not included in the original request our request fails (Flask requires content type to be present by default)
+
+    res = requests.post(url, headers={'Content-Type': 'application/json'})
     res.raise_for_status()  # Raise an exception if the request was not successful (status code >= 400)
   except requests.exceptions.RequestException as e:
-    logging.error(f"Failed to make a request to {url}: {e}")
+    logging.error(f"Failed to make a request to {url}: {e}") # Catch request errors separately
   except Exception as e:
-      logging.error(f"An error occurred in async_request: {e}")
+      logging.error(f"An error occurred in async_request: {e}") # Catch any other errors
 
 @app.route('/webhooks/<path:subpath>', methods=['POST'])
 def webhook(subpath):
-  dst_url = os.environ.get("RELAY_DST_URL")
-  data    = request.get_json()
-  url     = f"{dst_url}/{subpath}"
-
-  thread  = threading.Thread(target=async_request, args=(url,))
-  thread.start()
-
-  return jsonify(success=True), 200
-
-if __name__ == '__main__':
   try:
-    app.run(host=os.environ.get("RELAY_HOST", "0.0.0.0"), port=os.environ.get("RELAY_PORT", 50000))
+    dst_url = os.environ.get("RELAY_DST_URL")
+    data    = request.get_json()
+    url     = f"{dst_url}/{subpath}"
+
+    thread  = threading.Thread(target=async_request, args=(url,))
+    thread.start()
+
+    return jsonify(success=True), 200
   except Exception as e:
     logging.error(f"An error occurred in the main thread: {e}")
+
+if __name__ == '__main__':
+  app.run(host=os.environ.get("RELAY_HOST", "0.0.0.0"), port=os.environ.get("RELAY_PORT", 50000))
